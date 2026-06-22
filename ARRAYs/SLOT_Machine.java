224ad8faada88706808b74e6d31c425bba26ac12
Souravroy99 <souravroy200418@gmail.com>
2026-06-22 13:55:20 +0530

    Working on slot

---
+++ ARRAYs/SLOT_Machine.java
@@ -0,0 +1,20 @@
+import java.util.Scanner ;
+import java.util.Random ;
+
+public class SLOT_Machine
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in) ;
+        Random rand = new Random() ;
+
+        int balance = 100, bet, payout ;
+        String[] row;
+
+        System.out.println("Welcome to Java Slots");
+        System.out.println("Symbols: @ # $ % &") ;
+
+        
+
+    }
+}
