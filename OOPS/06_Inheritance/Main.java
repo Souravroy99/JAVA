5ccd21757e6c85647cd50dddb4bb067d84bb32e5
Souravroy99 <souravroy200418@gmail.com>
2026-06-28 16:30:09 +0530

    Single level inheritance

---
+++ OOPS/06_Inheritance/Main.java
@@ -0,0 +1,19 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Dog dog = new Dog() ;
+        Cat cat = new Cat() ;
+
+        System.out.println(dog.isAlive);
+        System.out.println(dog.lives);
+        dog.eat();
+        dog.speak();
+
+
+        System.out.println(cat.isAlive);
+        System.out.println(cat.lives);
+        cat.eat();
+        cat.speak();
+    }
+}
