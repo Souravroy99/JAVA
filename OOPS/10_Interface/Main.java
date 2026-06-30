dea963cd572beca9faa3a5e96b0037aab94eb9be
Souravroy99 <souravroy200418@gmail.com>
2026-06-30 22:15:10 +0530

    Need to learn interface

---
+++ OOPS/10_Interface/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    // Interface --> A blueprint for a class that specifies a set of abstract methods that implementing classes must define .
+    // Supports multiple inheritance-like behaviour
+
+    public static void main(String[] args)
+    {
+        Rabbit rabbit = new Rabbit() ;
+        
+    }
+}
---
+++ OOPS/10_Interface/Rabbit.java
@@ -0,0 +1,6 @@
+public class Rabbit implements Prey
+{
+    void flee() {
+        System.out.println("Rabbit flee");
+    }
+}
